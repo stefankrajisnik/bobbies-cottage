5ad10fccadc16f5802bd3b8b499003dc6ffdf22d
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-03-30 21:50:57 +0200

    Adding animation at navigation, also working at gallery section

--- script/main.js
+++ script/main.js
@@ -9,3 +9,12 @@ winterGallery.forEach((photo, index) => {
     photo.style.height = `${sizePixel}px`;
     photo.style.width = `${sizePixel}px`;
 });
+
+const nav = document.querySelector('.navigation')
+const topNavigation = document.querySelector('.top-navigation');
+
+window.addEventListener('scroll', fixNavigation)
+
+function fixNavigation() {
+    window.scrollY > 32 ? nav.classList.add('active') : nav.classList.remove('active');
+}
