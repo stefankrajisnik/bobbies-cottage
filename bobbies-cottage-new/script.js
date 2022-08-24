78a89d601a4b3aac6989ded087e1107531b252e8
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-08-24 19:41:01 +0200

    Everything started again, working on first section

---
+++ bobbies-cottage-new/script.js
@@ -0,0 +1,13 @@
+const text = document.querySelector('.div-text p');
+
+// split the text into letters and put them into the span elements
+text.innerHTML = text.innerText.split('').map((letter,i) => 
+     `<span class="span">${letter}</span>`
+).join('');
+
+// grab all spans and make them rotate 
+const allSpans = document.querySelectorAll('span');
+console.log(allSpans)
+allSpans.forEach((span,idx) => {
+    span.style.transform = `rotate(${idx * 11.5}deg)`
+})
